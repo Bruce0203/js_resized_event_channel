27b315307c2df3aa9978ab7bb4ba08f6005f2822
Bruce0203 <BruceFreedy@gmail.com>
2024-10-12 12:49:12 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use kanal::{AsyncReceiver, AsyncSender};
+use winit::dpi::PhysicalSize;
+
+pub struct JsResizeEventChannel {
+    receiver: AsyncReceiver<()>,
+}
+
+impl JsResizeEventChannel {
+    pub fn init() -> Self {
+        let (sender, receiver) = kanal::unbounded_async();
+        Self::register_resize_event_from_js(sender);
+        Self { receiver }
+    }
+
+    fn register_resize_event_from_js(sender: AsyncSender<()>) {
+        let f = wasm_bindgen::prelude::Closure::wrap(Box::new(move || {
+            pollster::block_on(sender.send(())).unwrap();
+        }) as Box<dyn FnMut()>);
+        web_sys::window()
+            .unwrap()
+            .set_onresize(Some(wasm_bindgen::JsCast::unchecked_ref(f.as_ref())));
+        f.forget();
+    }
+
+    pub fn try_recv_resized_event(&self) -> Option<PhysicalSize<u32>> {
+        if let Ok(Some(())) = self.receiver.try_recv() {
+            let window = web_sys::window().unwrap();
+            macro_rules! to_f64_from_js_value {
+                ($size:expr) => {
+                    $size.unwrap().as_ref().as_f64().unwrap()
+                };
+            }
+            let size = PhysicalSize::new(
+                to_f64_from_js_value!(window.inner_width()) as u32 * 2,
+                to_f64_from_js_value!(window.inner_height()) as u32 * 2,
+            );
+            Some(size)
+        } else {
+            None
+        }
+    }
+}
