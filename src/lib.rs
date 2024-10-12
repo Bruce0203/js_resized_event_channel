34b2be5f2f22a7876ae061baf6f670adeda410ba
Bruce0203 <BruceFreedy@gmail.com>
2024-10-12 13:49:06 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,48 @@ pub struct JsResizeEventChannel {
     receiver: AsyncReceiver<()>,
 }
 
+macro_rules! to_f64_from_js_value {
+    ($size:expr) => {
+        $size.unwrap().as_ref().as_f64().unwrap() as u32
+    };
+}
+
 impl JsResizeEventChannel {
-    pub fn init() -> Self {
+    pub fn init(window: &winit::window::Window) -> Self {
         cfg_if! {
             if #[cfg(target_arch = "wasm32")] {
                 let (sender, receiver) = kanal::unbounded_async();
                 Self::register_resize_event_from_js(sender);
+                Self::setup_canvas(window);
                 Self { receiver }
             } else {
                 Self {}
             }
+        }
+    }
+
+    pub fn try_resize_event(&self, window: &winit::window::Window) {
+        if let Some(size) = self.try_recv_resized_event() {
+            let _ = window.request_inner_size(size);
+        }
+    }
+
+    fn setup_canvas(window: &winit::window::Window) {
+        #[cfg(target_arch = "wasm32")]
+        {
+            let canvas = winit::platform::web::WindowExtWebSys::canvas(window).unwrap();
+            web_sys::window()
+                .and_then(|win| {
+                    let doc = win.document()?;
+                    let dst = doc.get_element_by_id("container")?;
+                    dst.append_child(&canvas).ok()?;
+                    std::hint::black_box(window.request_inner_size(winit::dpi::PhysicalSize::new(
+                        to_f64_from_js_value!(win.inner_width()),
+                        to_f64_from_js_value!(win.inner_height()),
+                    )));
+                    Some(())
+                })
+                .expect("Couldn't append canvas to document body.");
         }
     }
 
@@ -33,19 +65,14 @@ impl JsResizeEventChannel {
         }
     }
 
-    pub fn try_recv_resized_event(&self) -> Option<PhysicalSize<u32>> {
+    fn try_recv_resized_event(&self) -> Option<PhysicalSize<u32>> {
         cfg_if! {
             if #[cfg(target_arch = "wasm32")] {
                 if let Ok(Some(())) = self.receiver.try_recv() {
                     let window = web_sys::window().unwrap();
-                    macro_rules! to_f64_from_js_value {
-                        ($size:expr) => {
-                            $size.unwrap().as_ref().as_f64().unwrap()
-                        };
-                    }
                     let size = PhysicalSize::new(
-                        to_f64_from_js_value!(window.inner_width()) as u32 * 2,
-                        to_f64_from_js_value!(window.inner_height()) as u32 * 2,
+                        to_f64_from_js_value!(window.inner_width()) * 2,
+                        to_f64_from_js_value!(window.inner_height()) * 2,
                     );
                     Some(size)
                 } else {
