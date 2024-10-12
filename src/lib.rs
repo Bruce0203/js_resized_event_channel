b87b16a2a9d5c5560301854f46d7cdd86099f6ed
Bruce0203 <BruceFreedy@gmail.com>
2024-10-12 19:21:40 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,14 @@ use winit::dpi::PhysicalSize;
 
 pub struct JsResizeEventChannel {
     receiver: AsyncReceiver<()>,
-    canvas: HtmlCanvasElement,
 }
 
 impl JsResizeEventChannel {
     pub fn init(window: &winit::window::Window, dst: Element) -> Self {
         let (sender, receiver) = kanal::unbounded_async();
-        let canvas = Self::setup_canvas(window, dst);
-        Self::register_resize_event_to_js(&canvas, sender);
-        Self { receiver, canvas }
+        Self::setup_canvas(window, dst);
+        Self::register_resize_event_to_js(sender);
+        Self { receiver }
     }
 
     pub fn try_recv_resized_event(&self) -> bool {
@@ -31,19 +30,19 @@ impl JsResizeEventChannel {
         }
     }
 
-    fn setup_canvas(window: &winit::window::Window, dst: Element) -> HtmlCanvasElement {
+    fn setup_canvas(window: &winit::window::Window, dst: Element) {
         let canvas = winit::platform::web::WindowExtWebSys::canvas(window).unwrap();
         dst.append_child(&canvas).expect("Cannot append canvas");
 
         let _ = window.request_inner_size(size_of_window());
-        canvas
     }
 
-    fn register_resize_event_to_js(canvas: &HtmlCanvasElement, sender: AsyncSender<()>) {
+    fn register_resize_event_to_js(sender: AsyncSender<()>) {
         let f = wasm_bindgen::prelude::Closure::wrap(Box::new(move || {
             pollster::block_on(sender.send(())).unwrap();
         }) as Box<dyn FnMut()>);
-        canvas.set_onresize(Some(wasm_bindgen::JsCast::unchecked_ref(f.as_ref())));
+        let window = web_sys::window().unwrap();
+        window.set_onresize(Some(wasm_bindgen::JsCast::unchecked_ref(f.as_ref())));
         f.forget();
     }
 
