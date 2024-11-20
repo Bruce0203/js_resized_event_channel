daaf1d99f2cd8e250b64c507ad00f10823f5bf89
Bruce0203 <BruceFreedy@gmail.com>
2024-11-20 21:01:29 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@ impl JsResizeEventChannel {
             let entry = entries.at(0);
             let entry: ResizeObserverEntry = entry.dyn_into().unwrap();
             let size: ResizeObserverSize = entry.content_box_size().at(0).dyn_into().unwrap();
-            let size = PhysicalSize::new(size.block_size() as u32, size.inline_size() as u32);
+            let size = PhysicalSize::new(size.inline_size() as u32, size.block_size() as u32);
             let canvas = entry.target();
             let width = size.width.to_string();
             let height = size.height.to_string();
